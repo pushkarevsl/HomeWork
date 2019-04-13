bb87966c6fd9dbe2b70970f1b7c1e81afab249f3
pushkarevsl <pqi2gb@mail.ru>
2019-04-13 18:59:28 +0700

    Понял что нужно делать. Берем значение наше и прибавляем и отнимаем сначало по 50, 25, 10, 3, 1. Создавая коридор. И сравнивать наше значение с рандомным. А на выводе горячо, Горячее, очень и тд p.s Что на for что на while материться при commit

--- src/ru/pushkarev/task2/TaskWithStar.java
+++ src/ru/pushkarev/task2/TaskWithStar.java
@@ -22,20 +22,39 @@ import java.io.InputStreamReader;
     Через Баффер ввод с консоли
 3. Короче делим 100 попполам и смотрим если от 0 до 50 то...., если от50 до 100 то...., и так для каждый
    отрезок делим пополам
+4. Понял что нужно делать. Берем значение наше и прибавляем и отнимаем сначало по 50, 25, 10, 3, 1. Создавая коридор.
+И сравнивать наше значение с рандомным. А на выводе горячо, Горячее, очень и тд
 
 */
 public class TaskWithStar {
     public static void main(String[] args) throws IOException {
+        System.out.print("Рандомное число = ");
         int rnd = (int) (Math.random() * 100 + 1);
         System.out.println(rnd); //вывел для себя, чтоб знать и ориентироваться
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Введите число от 1 до 100");
 
-        int val = Integer.parseInt(reader.readLine());
 
+        for (; ; ) {
+            int val = Integer.parseInt(reader.readLine());
 
-
+            if (rnd < (val + 50) && rnd > (val + 50)) {
+                System.out.println("Тепло");
+            }
+            if (rnd < (val + 25) && rnd > (val + 25)) {
+                System.out.println("Теплее");
+            }
+            if (rnd < (val + 10) && rnd > (val + 10)) {
+                System.out.println("Горячо");
+            }
+            if (rnd < (val + 3) && rnd > (val + 3)) {
+                System.out.println("Почти обжегся!!!!");
+            }
+            if (rnd == val) {
+                System.out.println("You winner!!!!");
+            }
+        }
     }
 }
 
