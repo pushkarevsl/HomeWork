f94356e6eabfefea86c9356276dd068095e90108
pushkarevsl <pqi2gb@mail.ru>
2019-04-13 11:04:57 +0700

    Пробелов, разделителей добавил, думаю так читаться лучше будет, а не сплошным текстом

--- src/ru/pushkarev/task1/Task1.java
+++ src/ru/pushkarev/task1/Task1.java
@@ -2,8 +2,10 @@ package ru.pushkarev.task1;
 
 //программу, которая считает стоимость бензина (на вход программе передается кол-во литров, на выходе печатается
 // стоимость). Пример: стоимость литра бензина 43 рубля. На вход подается 50, на выходе должно быть 2150 руб.
+
 public class Task1 {
     public static void main(String[] args) {
+
         int quantity = 50;
         int valueOneLitre = 43;
         System.out.println("Стоимость бензина равна " + quantity * valueOneLitre + " руб.");
--- src/ru/pushkarev/task1/Task3.java
+++ src/ru/pushkarev/task1/Task3.java
@@ -1,10 +1,11 @@
 package ru.pushkarev.task1;
 
 //программу, которая конвертирует секунды в часы. Пример: на вход подается 3600, на выходе печатается 1 час.
+
 public class Task3 {
     public static void main(String[] args) {
-        int seconds = 3600;
 
+        int seconds = 3600;
         double hour = (seconds * 1.0) / 3600;
         System.out.println(seconds + " секунд составляют " + hour + " час(а)");
     }
