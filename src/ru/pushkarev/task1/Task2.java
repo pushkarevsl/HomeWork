72335958ff1cf68be4b44182326140512a45f1ee
pushkarevsl <pqi2gb@mail.ru>
2019-04-14 21:37:40 +0700

    Прдкорректировал задачи, согласно замечаниям. Ввел дополнительные переменные, чтоб не было в выводе решения

--- src/ru/pushkarev/task1/Task2.java
+++ src/ru/pushkarev/task1/Task2.java
@@ -1,14 +1,13 @@
 package ru.pushkarev.task1;
-
+//
 //программу,которая считает зарплату «на руки» (на вход программе передается величина зарплаты,
 // на выходе печатается зарплата за вычетом 13%(НДФЛ).Пример:на вход подается 70000,на выходе печатается 60900руб.
 
 public class Task2 {
-
     public static void main(String[] args) {
-
-        int salaryBeforeTax = 70000;
-        double tax = 0.87;
-        System.out.println("Зарплата после подоходного налога в 13%, составляет " + tax * salaryBeforeTax + " рублей");
+        int salaryBeforeTax = 1111;
+        int tax = 87;
+        double salaryAfterTax = (double) tax * salaryBeforeTax / 100;
+        System.out.println("Зарплата после подоходного налога в 13%, составляет " + salaryAfterTax + " рублей");
     }
 }
--- src/ru/pushkarev/task1/Task3.java
+++ src/ru/pushkarev/task1/Task3.java
@@ -1,13 +1,10 @@
 package ru.pushkarev.task1;
 
 //программу, которая конвертирует секунды в часы. Пример: на вход подается 3600, на выходе печатается 1 час.
-
 public class Task3 {
-
     public static void main(String[] args) {
-
-        int seconds = 3600;
-        double hour = (seconds * 1.0) / 3600;
+        int seconds = 3609;
+        double hour = (double) seconds / 3600;
         System.out.println(seconds + " секунд составляют " + hour + " час(а)");
     }
 }
