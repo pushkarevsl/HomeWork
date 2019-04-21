532fc40c01f43d2295eb9b3c5b0af8d66d7662d3
pushkarevsl <pqi2gb@mail.ru>
2019-04-22 01:07:25 +0700

    Все под снос. наинаю по новой, запутался

---
+++ src/ru/pushkarev/homeWork_19_04/appliancesStorage/ClassAppliances.java
@@ -0,0 +1,7 @@
+package ru.pushkarev.homeWork_19_04.appliancesStorage;
+
+public class ClassAppliances {
+    private String titleClass; //название класса техники
+
+
+}
--- src/ru/pushkarev/homeWork_19_04/appliancesStorage/TypeAppliances.java
+++ src/ru/pushkarev/homeWork_19_04/appliancesStorage/TypeAppliances.java
@@ -1,13 +1,15 @@
 package ru.pushkarev.homeWork_19_04.appliancesStorage;
 
-
 public class TypeAppliances {
 
-    private String type; //   название типов быт техники
-    private Refregarator[] refregarators;
+    private String title; // название типов
+    private TitleName[] titleNames = new TitleName[10]
 
+    public String getTitle() {
+        return title;
+    }
 
-    public TypeAppliances() {
-        refregarators = new Refregarator[];
+    public void setTitle(String title) {
+        this.title = title;
     }
 }
