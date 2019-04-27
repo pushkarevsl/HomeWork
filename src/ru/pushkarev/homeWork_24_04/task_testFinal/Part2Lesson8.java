34c38b308c7bbc2207a3d8ace01a76ccf025daa4
pushkarevsl <pqi2gb@mail.ru>
2019-04-27 13:06:33 +0700

    Создал пакет для Теста. Поигрался с final. Начинаю делать конвертер

---
+++ src/ru/pushkarev/homeWork_24_04/task_testFinal/Part2Lesson8.java
@@ -0,0 +1,11 @@
+package ru.pushkarev.homeWork_24_04.task_testFinal;
+
+import ru.pushkarev.homeWork_24_04.task_testFinal.Lesson8;
+
+public class Part2Lesson8 extends Lesson8 {
+
+    @Override
+    public void test() {
+        System.out.println("Пока");
+    }
+}
