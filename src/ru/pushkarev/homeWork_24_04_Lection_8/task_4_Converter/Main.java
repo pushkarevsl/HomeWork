ec91cfd67d54efc6f5064862856eaca9ef478415
pushkarevsl <pqi2gb@mail.ru>
2019-04-29 20:44:57 +0700

    Задача №4. Акт конвертирует, но убрал массив из аргументов. И щас попробую сканером еще поля заполнять.

--- src/ru/pushkarev/homeWork_24_04_Lection_8/task_4_Converter/Main.java
+++ src/ru/pushkarev/homeWork_24_04_Lection_8/task_4_Converter/Main.java
@@ -3,6 +3,8 @@ package ru.pushkarev.homeWork_24_04_Lection_8.task_4_Converter;
 import ru.pushkarev.homeWork_24_04_Lection_8.task_4_Converter.Contract;
 import ru.pushkarev.homeWork_24_04_Lection_8.task_4_Converter.Converter;
 
+import java.util.Scanner;
+
 
 /**
  * 4. Реализовать два класса: договор и акт. В каждом сделать поля: номер, дата, список товаров (массив строк).
@@ -13,8 +15,9 @@ public class Main {
 
     public static void main(String[] args) {
 
+
         Contract contract;
-        contract = new Contract(101, 29, "zz");
+        contract = new Contract(234, 23, "dfsdf");
         System.out.println(Converter.convert(contract));
 
     }
