1402eaa2339d169dc74de40b9c52fa4338f30bd3
pushkarevsl <pqi2gb@mail.ru>
2019-04-29 20:27:39 +0700

    Задача №4. Акт конвертирует, но убрал массив из аргументов. И щас попробую сканером еще поля заполнять.

--- src/ru/pushkarev/homeWork_24_04_Lection_8/task_4_Converter/Act.java
+++ src/ru/pushkarev/homeWork_24_04_Lection_8/task_4_Converter/Act.java
@@ -5,9 +5,46 @@ import java.time.LocalDate;
 
 public class Act {
 
-    public static int idNumber;
-    public static LocalDate date;
-    public static String provider;
-    public static String client;
+    private int idNumber;
+    private int data;
+    private String titleProduct;
 
+    @Override
+    public String toString() {
+        return "Act{" +
+                "idNumber=" + idNumber +
+                ", data=" + data +
+                ", titleProduct='" + titleProduct + '\'' +
+                '}';
+    }
+
+    public int getIdNumber() {
+        return idNumber;
+    }
+
+    public void setIdNumber(int idNumber) {
+        this.idNumber = idNumber;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public String getTitleProduct() {
+        return titleProduct;
+    }
+
+    public void setTitleProduct(String titleProduct) {
+        this.titleProduct = titleProduct;
+    }
+
+    public Act(int idNumber, int data, String titleProduct) {
+        this.idNumber = idNumber;
+        this.data = data;
+        this.titleProduct = titleProduct;
+    }
 }
--- src/ru/pushkarev/homeWork_24_04_Lection_8/task_4_Converter/Converter.java
+++ src/ru/pushkarev/homeWork_24_04_Lection_8/task_4_Converter/Converter.java
@@ -4,8 +4,11 @@ public class Converter {
 
     public static void main(String[] args) {
 
-
     }
 
+    public static Act convert(Contract contract) {
+        Act aux = new Act(contract.getIdNumber(), contract.getData(), contract.getTitleProduct());
+        return aux;
+    }
 
 }
