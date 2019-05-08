c11dd0d12ce11c802c9a1f3121c4302363da3f15
pushkarevsl <pqi2gb@mail.ru>
2019-05-08 20:31:44 +0700

    Добавил валидацию на ввод

--- src/ru/pushkarev/string/AppString.java
+++ src/ru/pushkarev/string/AppString.java
@@ -19,16 +19,26 @@ public class AppString {
     public static void main(String[] args) {
 
         String s;
+        System.out.println("Введите текст со словом бяка");
         Scanner scanner = new Scanner(System.in);
         s = scanner.nextLine();
+// первым if валидируем с разными окончаниями (английский и русский языки)
+        if (s.contains("бяка") & s.contains("бяka")) {
+            String s2 = s.replaceAll("бяка", " «вырезано цензурой» ");
+            String s3 = s2.replaceAll("бяka", " «вырезано цензурой» ");
+            System.out.println(s3);
+            //последующие проверки. одинарные
+        } else if (s.contains("бяка")) {
+            String s2 = s.replaceAll("бяка", " «вырезано цензурой» ");
+            System.out.println(s2);
+        } else if (s.contains("бяka")) {
+            String s3 = s.replaceAll("бяka", " «вырезано цензурой» ");
+            System.out.println(s3);
+        } else {
+            System.out.println("Нету бяки");
 
-        String s2 = s.replaceAll("бяка", " «вырезано цензурой» ");
 
-        System.out.println(s2);
-
-
-// kacklakc бяка воттстс бяка
-
-
+            //skvmsdkvm skmckm smбяка дульмллмьбяka
+        }
     }
 }
