ec6fb00f4d17cc8502edbff35f3deafe9105a385
pushkarevsl <pqi2gb@mail.ru>
2019-06-03 19:05:37 +0700

    Отделяю пустые значения массива

--- src/ru/pushkarev/homeWork_27_05_Bild/Main.java
+++ src/ru/pushkarev/homeWork_27_05_Bild/Main.java
@@ -1,7 +1,5 @@
 package ru.pushkarev.homeWork_27_05_Bild;
 
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Locale;
@@ -25,26 +23,67 @@ import java.util.Scanner;
  * products.txt
  */
 
+/**
+ * Решение. Что хочу сделать.
+ * 1й этап.
+ * - Объявляю два массива.
+ * Один String - сюда считывать будем названия.
+ * И один double, для цены за товар и количества.
+ * - В цикле while со сканером считываем значения из файла.
+ * если строка то заносим в массив строк
+ * иначе, то заносим в массив double
+ * 2й этап.
+ * Все данные спарсили. выводим в чек
+ */
+
 public class Main {
+
     public static void main(String[] args) {
+        String[] title = new String[50];
+        Double[] price = new Double[50];
+
+        System.out.printf("%-20s%-10s%-11s%-9s%n", "Наименование", "Цена", "Кол-во", "Стоимость");
+        System.out.printf("==================================================");
+        System.out.println(" ");
+
         try (FileReader fr = new FileReader("products.txt");
              Scanner scanner = new Scanner(fr)
         ) {
             scanner.useLocale(Locale.ENGLISH);
 
             while ((scanner.hasNext())) {
-
-                if (scanner.hasNextDouble()) {
-                    double d = scanner.nextDouble();
-                    System.out.printf("%-10s: %f%n", "double", d);
-                } else {
-                    String s = scanner.next();
-                    System.out.printf("%-10s: %s%n", "String", s);
+                for (int i = 0; i < title.length; i++) {
+                    if (scanner.hasNextDouble()) {
+                        double d = scanner.nextDouble();
+                        price[i] = d;
+                        //System.out.println(d);
+                    } else if (scanner.hasNextLine()) {
+                        String s = scanner.nextLine();
+                        title[i] = s;
+                        //System.out.println(s);
+                    }
                 }
             }
 
         } catch (IOException e) {
             e.printStackTrace();
         }
+        for (int i = 0; i < title.length; i++) {
+            if (title[i] != null) {
+                System.out.println(title[i]);
+            }
+        }
+        double t = 0;
+        for (int i = 0; i < price.length + 1; i++) {
+            if (price[i] == null) {
+                price[i] = t;
+                price[i] = price[i + 1];
+
+
+            }
+        }
+
+
+        // System.out.printf("%-30s%n", "Итого: ");
     }
 }
