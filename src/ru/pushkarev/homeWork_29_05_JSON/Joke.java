555c86bec03e3ce4c6743133fb48b77d6bc24238
pushkarevsl <pqi2gb@mail.ru>
2019-06-05 19:29:31 +0700

    Делаю вывод JSON

---
+++ src/ru/pushkarev/homeWork_29_05_JSON/Joke.java
@@ -0,0 +1,54 @@
+package ru.pushkarev.homeWork_29_05_JSON;
+
+public class Joke {
+
+    private int id;
+    private String type;
+    private String setup;
+    private String punchline;
+
+
+    @Override
+    public String toString() {
+        return "Joke{" +
+                "id=" + id +
+                ", type='" + type + '\'' +
+                ", setup='" + setup + '\'' +
+                ", punchline='" + punchline + '\'' +
+                '}';
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getSetup() {
+        return setup;
+    }
+
+    public void setSetup(String setup) {
+        this.setup = setup;
+    }
+
+    public String getPunchline() {
+        return punchline;
+    }
+
+    public void setPunchline(String punchline) {
+        this.punchline = punchline;
+    }
+}
+
+
