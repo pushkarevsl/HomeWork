5218b2e6afc07b30978bc3a66364619d092346bd
pushkarevsl <pqi2gb@mail.ru>
2019-06-07 19:58:01 +0700

    Вторая

--- src/ru/pushkarev/homeWork_31_05_Arrays/TaskTwoReverse.java
+++ src/ru/pushkarev/homeWork_31_05_Arrays/TaskTwoReverse.java
@@ -1,5 +1,7 @@
 package ru.pushkarev.homeWork_31_05_Arrays;
 
+import java.util.Arrays;
+
 /**
  * ДЗ_21_1_2_Реверс массива
  * Задача: Имеется массив, нужно переставить элементы массива в обратном порядке.
@@ -10,4 +12,23 @@ package ru.pushkarev.homeWork_31_05_Arrays;
  */
 
 public class TaskTwoReverse {
+
+    public static void main(String[] args) {
+        int[] array = new int[10];
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i + 1;
+        }
+
+        int[] copyArray = new int[array.length];
+
+        for (int i = array.length - 1; i >= 0; i--) {
+            copyArray[array.length - i - 1] = array[i];
+        }
+        System.out.println (" ");
+
+        System.out.println (Arrays.toString (array));
+        System.out.println (Arrays.toString (copyArray));
+
+    }
 }
