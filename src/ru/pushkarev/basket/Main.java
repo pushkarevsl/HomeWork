09d9b717aafa0f8ccf8b5ccd5a93d0289c306660
pushkarevsl <pqi2gb@mail.ru>
2019-06-09 15:54:19 +0700

    Создал main :)

---
+++ src/ru/pushkarev/basket/Main.java
@@ -0,0 +1,17 @@
+package ru.pushkarev.basket;
+
+/**
+ * Реализовать класс корзины интернет магазина по следующему интерфейсу:
+ * <p>
+ * interface Basket {
+ * <p>
+ * void addProduct(String product, int quantity);
+ * void removeProduct(String product);
+ * void updateProductQuantity(String product, int quantity);
+ * void clear();
+ * List<String> getProducts();
+ * int getProductQuantity(String product);
+ */
+
+public class Main {
+}
