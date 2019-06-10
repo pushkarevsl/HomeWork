659b03328a6bf9b44782c5244d5a3ce13d5ef4a9
pushkarevsl <pqi2gb@mail.ru>
2019-06-10 19:06:56 +0700

    Создал main :)

--- src/ru/pushkarev/homeWork_MyException/homeWork_13_05_Exceptiop_part_2/Main.java
+++ src/ru/pushkarev/homeWork_MyException/homeWork_13_05_Exceptiop_part_2/Main.java
@@ -19,6 +19,7 @@ package ru.pushkarev.homeWork_MyException.homeWork_13_05_Exceptiop_part_2;
 
 public class Main {
 
+
     public static void main(String[] args) {
 
         Child child = new Child();
