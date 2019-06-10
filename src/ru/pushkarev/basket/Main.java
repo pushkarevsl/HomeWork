40ab5a65cee7db428701a5cd271566747bb52c9c
pushkarevsl <pqi2gb@mail.ru>
2019-06-10 19:33:08 +0700

    Создал main :)

--- src/ru/pushkarev/basket/Main.java
+++ src/ru/pushkarev/basket/Main.java
@@ -1,17 +1,29 @@
 package ru.pushkarev.basket;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Реализовать класс корзины интернет магазина по следующему интерфейсу:
  * <p>
  * interface Basket {
  * <p>
- * void addProduct(String product, int quantity);
- * void removeProduct(String product);
- * void updateProductQuantity(String product, int quantity);
- * void clear();
- * List<String> getProducts();
- * int getProductQuantity(String product);
+ * void addProduct(String product, int quantity);               //добавить продукт и его количество
+ * void removeProduct(String product);                          //удалить только продукт
+ * void updateProductQuantity(String product, int quantity);    //обновить количество продукта
+ * void clear();                                                //очистка корзины
+ * List<String> getProducts();                                  //достань продукт
+ * int getProductQuantity(String product);                      //достань количество продукта
+ * }
  */
 
-public class Main {
+public class Main implements Basketable {
+    List<String> product = new ArrayList<> ( );
+    List<Integer> quantity = new ArrayList<> ( );
+
+
+    @Override
+    public void addProduct(String product, int quantity) {
+
+    }
 }
